883aab717feae3c803ba9845c9f4fb61976ed3cb
cafragachan <cafragachan@yahoo.com>
2019-10-26 14:02:05 +0100

    Initial commit

---
+++ Source/SCTest/SCTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SCTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCTEST_API ASCTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
